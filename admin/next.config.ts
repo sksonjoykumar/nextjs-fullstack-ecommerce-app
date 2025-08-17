3a4be620e76b091be125349ed88467948e8c55ef
sksonjoykumar <sksonjoykumar111@gmail.com>
2025-08-18 01:18:32 +0600

    added

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,21 +1,12 @@
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         hostname: 'res.cloudinary.com',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        hostname: 'res.cloudinary.com',
+        hostname: "res.cloudinary.com",
       },
     ],
   },
